5684405e0ebcf7b47f38d6a31ec0649918e47189
ViktoriaV <august.flamenco@gmail.com>
2023-11-06 17:10:24 +0300

    correction

--- posts/index.js
+++ posts/index.js
@@ -3,5 +3,3 @@
 
 window.open("https://oauth.vk.com/authorize?client_id=51786833&display=page&redirect_uri=https://viktoriav-start.github.io/posts/posts.html&scope=friends&response_type=token&v=:version&state=123456");
 
-
-
---
+++ posts/posts.js
@@ -0,0 +1,19 @@
+async function getData() {
+
+  const response = await fetch('http://api.vk.com/method/get.wall', {
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json",
+    },
+
+    body: JSON.stringify({
+      access_token: 'vk1.a.DoZLc-3Uup1JZW5HqbSjFg_RBxYh3PIZ9ol1igw15x5B2ibWSKU3jqPiCE7jxc8OuWwADzs6JkdlHn5dxDmiKNRx59WQPssrSqxA7hRYQfaps3GR8sGJ08lKpWP8ZG94AxCKLWZX087_FwpYt80FrE0nzZM7M0AjdrEGL_oonZex7tRqLgQhUjW4UVn1yQU5',
+      owner_id: '-33276697',
+      count: 3
+    }),
+  });
+  console.log(response);
+}
+
+getData();
