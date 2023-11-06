a813e110a062efe9cf6be841666a6065f4c1354e
ViktoriaV <august.flamenco@gmail.com>
2023-11-06 15:46:47 +0300

    correction

--- posts/index.js
+++ posts/index.js
@@ -1,4 +1,7 @@
 
 
 
-window.open("https://oauth.vk.com/authorize?client_id=51786833&display=page&redirect_uri=https://viktoriav-start.github.io/posts/posts.html/&scope=friends&response_type=token&v=:version&state=123456");
+window.open("https://oauth.vk.com/authorize?client_id=51786833&display=page&redirect_uri=https://viktoriav-start.github.io/posts/posts.html&scope=friends&response_type=token&v=:version&state=123456");
+
+
+
