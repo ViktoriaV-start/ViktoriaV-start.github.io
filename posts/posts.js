816ea1a296eedbd90309f50b0b7a2171b924544f
ViktoriaV <august.flamenco@gmail.com>
2023-11-07 14:34:34 +0300

    ready

--- posts/posts.js
+++ posts/posts.js
@@ -1,19 +1,143 @@
-async function getData() {
+'use strict';
 
-  const response = await fetch('https://api.vk.com/method/get.wall', {
-    method: "POST",
+const posts = {};
+let counter = 0;
+let offset = 0;
+let tempCounter = 0;
 
-    headers: {
-      "Content-Type": "application/json",
-    },
 
-    body: JSON.stringify({
-      access_token: 'vk1.a.DoZLc-3Uup1JZW5HqbSjFg_RBxYh3PIZ9ol1igw15x5B2ibWSKU3jqPiCE7jxc8OuWwADzs6JkdlHn5dxDmiKNRx59WQPssrSqxA7hRYQfaps3GR8sGJ08lKpWP8ZG94AxCKLWZX087_FwpYt80FrE0nzZM7M0AjdrEGL_oonZex7tRqLgQhUjW4UVn1yQU5',
-      owner_id: '-33276697',
-      count: 3
-    }),
-  });
-  console.log(response);
+if (localStorage.length != 0) {
+  counter = localStorage.length-1;
+  offset = localStorage.length-1;
 }
 
-getData();
+/**
+ * Загрузить данные VK
+ */
+function addScript() {
+
+  let elem = document.createElement("script");
+
+  elem.src = `https://api.vk.com/method/wall.get?owner_id=-33276697&fields=bdate&access_token=vk1.a.DoZLc-3Uup1JZW5HqbSjFg_RBxYh3PIZ9ol1igw15x5B2ibWSKU3jqPiCE7jxc8OuWwADzs6JkdlHn5dxDmiKNRx59WQPssrSqxA7hRYQfaps3GR8sGJ08lKpWP8ZG94AxCKLWZX087_FwpYt80FrE0nzZM7M0AjdrEGL_oonZex7tRqLgQhUjW4UVn1yQU5&offset=${offset}&count=100&v=5.131&callback=onVkData`;
+  document.querySelector('body').insertAdjacentElement('beforeend', elem);
+}
+
+addScript();
+
+/**
+ * Обработать пришедшие данные VK
+ */
+function onVkData(res) {
+  let result = res.response.items;
+  let length = result.length;
+
+  for (let i = 0; i <length; i++ ) {
+    posts[i] = result[i].text
+  }
+  insertPosts();
+}
+
+// Контейнер для постов на странице
+let scrollContainer = document.querySelector('.scrollContainer');
+
+// Слушатель события скрола, вызвать функцию обработчик
+document.querySelector('.main__content').addEventListener('scroll', scrollContent);
+
+/**
+ * Отследить скролл и подгрузить посты. Запросить новый пакет постов VK, если имеющиеся в posts уже загружены на страницу.
+ */
+function scrollContent() {
+
+  if (document.querySelector('.main__content').scrollTop + 500 >= document.querySelector('.main__content').scrollHeight) {
+
+    if (Object.keys(posts).length > tempCounter + 1) {
+      insertPosts();
+    }
+
+    if (tempCounter == 100 ){
+      console.log('Новый запрос');
+      tempCounter = 0;
+      offset = counter;
+      addScript();
+    }
+  }
+}
+
+// Флаг - отмечает загрузку всех имевшихся при открытии страницы постов из localStorage
+let isLocalStorageRendered = false;
+
+/**
+ * Добавление постов на страницу
+ */
+function insertPosts() {
+  let postsMarkup = '';
+
+// При первом входе
+// if (localStorage.length == 0 || (localStorage.length != 0 && isLocalStorageRendered && offset == 0)) {
+    if (offset == 0) {
+    console.log('Первичный вход')
+    for (let i = 0; i < 5; i++) {
+      postsMarkup += getPostMarkup(tempCounter);
+      localStorage[counter] = posts[tempCounter];
+      localStorage.counter = counter;
+      counter++;
+      tempCounter++;
+    }
+    isLocalStorageRendered = true;
+  }
+  
+  if (localStorage.length != 0 && !isLocalStorageRendered ) {
+    console.log('Рендер постов из localStorage')
+    for (let i = 0; i < localStorage.length; i++) {
+      if (localStorage.key(i) != 'counter') {
+        postsMarkup += getPostMarkupStorage(localStorage.key(i));
+      }
+    }
+    isLocalStorageRendered = true;
+
+  } else if (localStorage.length != 0 && isLocalStorageRendered && offset != 0) {
+      console.log('Рендер новых постов при offset != 0 и отображенном localStorage')
+
+      for (let i = 0; i < 5; i++) {
+        postsMarkup += getPostMarkup(tempCounter);
+
+        try {
+          localStorage[counter] = posts[tempCounter];
+          localStorage.counter = counter;
+        } catch (er) {
+          console.log(er);
+
+          let key = Object.keys(localStorage).sort((a, b) => a - b);
+          key = key.filter((el) => el != 'counter');
+
+          for (let i = 0; i < 10; i++) {
+            localStorage.removeItem(key[i]);
+          }
+        }
+        
+        counter++;
+        tempCounter++;
+      }
+  }
+
+  scrollContainer.insertAdjacentHTML('beforebegin', postsMarkup);
+}
+
+
+/**
+ * Разметка одного поста из объекта posts
+ * @param {number | string} idx
+ * @returns {string} html-разметка одного поста
+ */
+function getPostMarkup(idx) {
+  return `<div class="post">${posts[idx]}</div>`;
+}
+
+/**
+ * Разметка одного поста из localStorage
+ * @param {string} key
+ * @returns {string} html-разметка одного поста
+ */
+function getPostMarkupStorage(key) {
+  return `<div class="post">${localStorage[key]}</div>`;
+}
